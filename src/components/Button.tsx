0e8bb767750f946d94fd4d3904811d31f715142e
MaekawaMoikako <maekawamikako@gmail.com>
2022-08-23 15:24:44 +0900

    update: ボタンのレイアウト

---
+++ src/components/Button.tsx
@@ -0,0 +1,33 @@
+import { ButtonHTMLAttributes, DetailedHTMLProps } from "react";
+
+type ButtonHTMLProps = DetailedHTMLProps<
+  ButtonHTMLAttributes<HTMLButtonElement>,
+  HTMLButtonElement
+>;
+
+export const Button: React.VFC<ButtonHTMLProps> = ({
+  children,
+  onClick,
+  className,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={className}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: "12px 32px",
+        height: "40px",
+        background: "white",
+        border: "1px solid #ABABAB",
+        borderRadius: "8px",
+        fontSize: "16px",
+        color: "#767676",
+      }}
+    >
+      {children}
+    </button>
+  );
+};
