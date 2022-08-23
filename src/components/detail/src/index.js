0430ce5be6fb199d17f78d2125a05c12a04b2f69
KUGOUNagi <e8896@g.maizuru-ct.ac.jp>
2022-08-23 15:56:24 +0900

    詳細画面

--- src/components/detail/src/index.js
+++ src/components/detail/src/index.js
@@ -1,36 +1,77 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+  
+function Picture() {
+    return <img src={`test.png`} alt="Logo" />;
+}
 
-class Square extends React.Component {
+function Button1() {
+  return <button type="button" id="button"><img src={`reversetriangle.png`} alt="Logo" /></button>;
+}
+
+function Button2() {
+  return <button type="button" id="button"><img src={`triangle.png`} alt="Logo" /></button>;
+}
+
+class Person extends React.Component {
     render() {
         return (
             <div>
-                <h1>Hello! World.</h1>
-                <p>このページはテストページです。</p>
+                <h1>女性　10代</h1>
+                <p>福井県鯖江市新横江２丁目３−４</p>
             </div>
         );
     }
   }
-  
-  class Game extends React.Component {
+
+class Comment extends React.Component {
     render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Square />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
+        return (
+            <div>
+                <div>人形供養とかじゃないでめがね供養があった</div>
+            </div>
+        );
     }
+}
+
+class Heart extends React.Component {
+  render() {
+      return (
+        <div>❤200</div>
+      );
   }
+}
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="main">
+         <div className="main-picture">
+          <Button1/><Picture/><Button2/>
+        </div>
+        <div className="main-person">
+          <Person />
+        </div>
+        <div className="main-comment">
+          <Comment/>
+        </div>
+        <div className="main-heart">
+          <Heart/>
+        </div>
+      </div>
+    );
+  }
+}  
+  const Kard = () => {
+    return (
+      <Main/>
+    );
+  }
+  export default Kard;
   
   // ========================================
   
   const root = ReactDOM.createRoot(document.getElementById("root"));
-  root.render(<Game />);
+  root.render(<Main />);
   
