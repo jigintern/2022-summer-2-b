f55cb7d0b7e7e5dcd712c082516ccd2412b07c62
happy663 <53854298+happy663@users.noreply.github.com>
2022-08-23 11:21:11 +0900

    update: 地図の中心とマーカーを東京駅に変更

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,22 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+
+// @ts-ignore
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconUrl: markerIcon.src,
+  iconRetinaUrl: markerIcon2x.src,
+  shadowUrl: markerShadow.src,
+});
 
 const Map = () => {
   return (
     <MapContainer
-      center={[51.505, -0.09]}
+      center={[35.6809591, 139.7673068]}
       zoom={13}
       scrollWheelZoom={false}
       style={{ height: "100vh", width: "100%" }}
@@ -13,7 +25,7 @@ const Map = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[35.6809591, 139.7673068]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
