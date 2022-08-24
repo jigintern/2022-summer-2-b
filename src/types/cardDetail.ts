8f540ead649bf276315d5f98e42940663a903b12
happy663 <53854298+happy663@users.noreply.github.com>
2022-08-24 15:43:30 +0900

    update: cardDetailの一部を更新

--- src/types/cardDetail.ts
+++ src/types/cardDetail.ts
@@ -2,6 +2,6 @@ import { CardProps } from "src/types/card";
 
 export type CardDetailProps = {
   comment: string;
-  gender: string;
-  age: string;
+  gender: "男性" | "女性" | "その他";
+  age: "10代" | "20代" | "30代" | "40代" | "50代" | "60代" | "70代以上";
 } & CardProps;
