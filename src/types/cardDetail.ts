9b444d0c88118a8ec1f9318fbe2269e8da392ab3
happy663 <53854298+happy663@users.noreply.github.com>
2022-08-25 08:45:44 +0900

    update: 型のupdateと呼び出し元での調整

--- src/types/cardDetail.ts
+++ src/types/cardDetail.ts
@@ -1,7 +1,12 @@
 import { CardProps } from "src/types/card";
 
 export type CardDetailProps = {
+  reviews: Review[];
+} & CardProps;
+
+type Review = {
+  id: number;
   comment: string;
   gender: "男性" | "女性" | "その他";
   age: "10代" | "20代" | "30代" | "40代" | "50代" | "60代" | "70代以上";
-} & CardProps;
+};
