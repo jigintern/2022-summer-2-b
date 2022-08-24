3a74c9d01070fba8c29b2e90e2a8170081fe8aba
happy663 <53854298+happy663@users.noreply.github.com>
2022-08-24 12:52:28 +0900

    update: ファイル名変更

---
+++ src/pages/test.tsx
@@ -0,0 +1,9 @@
+import dynamic from "next/dynamic";
+import React from "react";
+import Detail from "src/components/Detail";
+
+function MapPage() {
+  return <Detail />;
+}
+
+export default MapPage;
