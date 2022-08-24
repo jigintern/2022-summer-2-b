1241ded8fc02226915efb5e589156c35871b0348
happy663 <53854298+happy663@users.noreply.github.com>
2022-08-24 14:03:04 +0900

    fix: again

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,9 +1,8 @@
 import dynamic from "next/dynamic";
 import React from "react";
-import Detail from "src/components/Detail";
 
 function Test() {
-  return <Detail />;
+  return <div />;
 }
 
 export default Test;
