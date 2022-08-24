cb27f26ab12db8a22567d43d9ec24f912eb811fb
62sorata <107105246+62sorata@users.noreply.github.com>
2022-08-24 14:24:46 +0900

    update

--- src/components/detail.tsx
+++ src/components/detail.tsx
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import './detail.css';
-
-const Detail = () => {
-return (
-  <div className="main">
-        <div className="main-heart">
-          <div>❤200</div>
-        </div>
-        <div className="main-picture">
-        <img src={`test.png`} alt="Logo" /> 
-        <button type="button" >
-        <img src={`triangle.png`} alt="Logo" id="button"/></button>
-        </div>
-        <div className="main-person">
-          <div>
-            <h1 
-            text-align:center>女性　10代</h1>
-            <p >福井県鯖江市新横江２丁目３−４</p>
-          </div>
-        </div>
-        <div className="main-comment">
-          <div>人形供養とかじゃないでめがね供養があった</div>
-        </div>
-        
-      </div>
-);
-}
-export default Detail;
-=======
 import Image from "next/image";
 import React from "react";
 import { Button } from "./Button";
@@ -108,4 +77,3 @@ const Detail = () => {
   );
 };
 export default Detail;
->>>>>>> b43e80bffe6b2cead0c680eb5f080544d9df0086
