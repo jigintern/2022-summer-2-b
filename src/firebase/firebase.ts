f0bf89840c62ca83c07e55501b0fe83d72407146
happy663 <53854298+happy663@users.noreply.github.com>
2022-08-25 18:01:57 +0900

    update: firebaseのコンフィグを更新

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,18 +1,24 @@
 import firebase from "firebase/app";
 import "firebase/storage";
+import "firebase/firestore";
 
 const firebaseConfig = {
   //confi
   apikey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAI,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECTT_ID,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.NEXT_PUBLIC_FIRBASE_MESSAGING_SENDER_ID,
   appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
   measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
+;
 
 //Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+if (firebase.apps.length === 0) {
+  firebase.initializeApp(firebaseConfig);
+}
 export const storage = firebase.storage();
+export const db = firebase.firestore();
 export default firebase;
