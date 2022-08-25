f8e7a3bb1fa192bcc8a3673a7420361f547dd062
happy663 <53854298+happy663@users.noreply.github.com>
2022-08-25 15:47:26 +0900

    update: data形式の変更とそれに伴う型の変更

--- src/types/submission.ts
+++ src/types/submission.ts
@@ -1,6 +1,7 @@
 import { CardProps } from "src/types/card";
 
 export type SubmissionProps = {
+  imgURL: string;
   comment: string;
   gender: "男性" | "女性" | "その他";
   age: "10代" | "20代" | "30代" | "40代" | "50代" | "60代" | "70代以上";
